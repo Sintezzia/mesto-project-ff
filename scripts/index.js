51e34a54ed09f49d5c6e05536023af5c4ebc4e9f
Влад Зильберберг <vladzilberberg@gmail.com>
2023-12-08 23:54:29 +0500

    сдача проекта

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content; 
 
 // @todo: DOM узлы
+const PlacesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(name, link, delCard) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = card.querySelector('.card__delete-button');
+
+  card.querySelector('.card__image').src = link;
+  card.querySelector('.card__image').alt = name;
+  card.querySelector('.card__title').textContent = name;
+
+  deleteButton.addEventListener('click', (event) => delCard(event));
+
+  return card;
+}
 
 // @todo: Функция удаления карточки
-
+function delCard(event) {
+  event.target.closest('.places__item').remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => PlacesList.appendChild(createCard(item.name, item.link, delCard)));
